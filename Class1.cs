485e15343f33d129359f31c5e550afefb8af67d0
ElMargaritox <alexisguzman0303@gmail.com>
2021-04-06 18:19:35 -0300

    OwO

---
+++ Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace RPNoDestroyVehicle
+{
+    
+    public class Class1 : RocketPlugin<Config>
+    {
+        public Class1 Instance;
+        protected override void Load()
+        {
+
+            Instance = this;
+            base.Load();
+            VehicleManager.onDamageVehicleRequested += VehiculoDaño;
+        }
+
+        private void VehiculoDaño(CSteamID instigatorSteamID, InteractableVehicle vehicle, ref ushort pendingTotalDamage, ref bool canRepair, ref bool shouldAllow, EDamageOrigin damageOrigin)
+        {
+            if (Configuration.Instance.vidainfinita)
+            {
+                pendingTotalDamage = 0;
+                shouldAllow = false;
+            }
+
+            if (Configuration.Instance.mostrarlog)
+            {
+                SteamPlayer nombre = PlayerTool.getSteamPlayer(instigatorSteamID);
+                
+                switch (damageOrigin)
+                {
+                    case EDamageOrigin.Useable_Gun:
+                        Logger.Log(nombre.playerID.characterName + " Esta Intentando Romper: " + vehicle.name.ToString());
+                        break;
+                    case EDamageOrigin.Useable_Melee:
+                        Logger.Log(nombre.playerID.characterName + " Esta Intentando Romper: " + vehicle.name.ToString());
+                        break;
+                    case EDamageOrigin.Punch:
+                        Logger.Log(nombre.playerID.characterName + " Esta Intentando Romper: " + vehicle.name.ToString());
+                        break;
+                }
+            }
+        }
+
+
+        protected override void Unload()
+        {
+            base.Unload();
+            VehicleManager.onDamageVehicleRequested -= VehiculoDaño;
+        }
+    }
+}
---
+++ Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API;
+
+namespace RPNoDestroyVehicle
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public bool vidainfinita;
+        public bool mostrarlog;
+        public void LoadDefaults()
+        {
+            vidainfinita = true;
+            mostrarlog = true;
+        }
+    }
+}
